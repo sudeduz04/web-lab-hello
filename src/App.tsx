cefa1dd13e9591800fd294980d2401f99ea7ce5c
Sude Düz <sudeduz04@gmail.com>
2026-02-24 14:17:11 +0300

    project created

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+<div>
+<h1>Web Tasarimi ve Programlama</h1>
+<h2>LAB-1</h2>
+<p>Ad Soyad: Sude Düz</p>
+<p>Ogrenci No: 230541046</p>
+</div>
+)
+}
+
+export default App
